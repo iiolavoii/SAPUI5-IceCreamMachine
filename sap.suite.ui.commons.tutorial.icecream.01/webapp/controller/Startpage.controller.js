36583e534e4ef4c60d0c749b0155b65e1fb94424
OlavoGomes <iiolavoii@gmail.com>
2021-04-21 10:33:10 +0100

    updated openui5 and finished STEP2

--- sap.suite.ui.commons.tutorial.icecream.01/webapp/controller/Startpage.controller.js
+++ sap.suite.ui.commons.tutorial.icecream.01/webapp/controller/Startpage.controller.js
@@ -1,37 +1,34 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel",
 	"sap/ui/core/format/NumberFormat",
 	"sap/base/strings/formatMessage"
-], function (JQuery, Controller, JSONModel, NumberFormat) {
+], function (Controller, NumberFormat, formatMessage) {
 	"use strict";
 
-	return Controller.extend("sap.suite.ui.commons.demokit.tutorial.icecream.01.controller.Startpage", {
-		onInit: function () {
-			var sDataPath = sap.ui.require.toUrl("sap/suite/ui/commons/demokit/tutorial/icecream/03/model/data") + "/News.json";
-			var oModel = new JSONModel(sDataPath);
-			this.getView().setModel(oModel, "news");
-		},
+	return Controller.extend("sap.suite.ui.commons.demokit.tutorial.icecream.02.controller.Startpage", {
+		onInit: function () {},
 
-		getProgress: function(aNodes) {
-			if(!aNodes || aNodes.length === 0){
+		formatMessage: formatMessage,
+
+		getProgress: function (aNodes) {
+			if (!aNodes || aNodes.length === 0) {
 				return 0;
 			}
 			var iSum = 0;
-			for (var i = 0; i < aNodes.length; i++){
+			for (var i = 0; i < aNodes.length; i++) {
 				iSum += aNodes[i].state === "Positive";
 			}
 			var fPercent = (iSum / aNodes.length) * 100;
 			return fPercent.toFixed(0);
 		},
 
-		getEntityCount: function(entities){
+		getEntityCount: function (entities) {
 			return entities && entities.length || 0;
 		},
-		
-		formatNumber: function(value){
+
+		formatNumber: function (value) {
 			var oFloatFormatter = NumberFormat.getFloatInstance({
-				style: "shor",
+				style: "short",
 				decimals: 1
 			});
 			return oFloatFormatter.format(value);
