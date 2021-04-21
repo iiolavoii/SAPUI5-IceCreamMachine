7a3dba469014239767efa1d2141690aa4def81fa
OlavoGomes <iiolavoii@gmail.com>
2021-04-21 10:15:25 +0100

    Initial Commit

---
+++ sap.suite.ui.commons.tutorial.icecream.01/webapp/controller/Startpage.controller.js
@@ -0,0 +1,40 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/format/NumberFormat",
+	"sap/base/strings/formatMessage"
+], function (JQuery, Controller, JSONModel, NumberFormat) {
+	"use strict";
+
+	return Controller.extend("sap.suite.ui.commons.demokit.tutorial.icecream.01.controller.Startpage", {
+		onInit: function () {
+			var sDataPath = sap.ui.require.toUrl("sap/suite/ui/commons/demokit/tutorial/icecream/03/model/data") + "/News.json";
+			var oModel = new JSONModel(sDataPath);
+			this.getView().setModel(oModel, "news");
+		},
+
+		getProgress: function(aNodes) {
+			if(!aNodes || aNodes.length === 0){
+				return 0;
+			}
+			var iSum = 0;
+			for (var i = 0; i < aNodes.length; i++){
+				iSum += aNodes[i].state === "Positive";
+			}
+			var fPercent = (iSum / aNodes.length) * 100;
+			return fPercent.toFixed(0);
+		},
+
+		getEntityCount: function(entities){
+			return entities && entities.length || 0;
+		},
+		
+		formatNumber: function(value){
+			var oFloatFormatter = NumberFormat.getFloatInstance({
+				style: "shor",
+				decimals: 1
+			});
+			return oFloatFormatter.format(value);
+		}
+	});
+});
